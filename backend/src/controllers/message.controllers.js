336824980776a2c34fdcfd655d8c2b9072722844
rohit <rohitsirsat855@gmail.com>
2024-06-24 15:09:23 +0530

    add: message controller

---
+++ backend/src/controllers/message.controllers.js
@@ -0,0 +1,214 @@
+import mongoose from "mongoose";
+import { ChatEventEnum } from "../constants.js";
+import { Chat } from "../models/chat.models.js";
+import { ChatMessage } from "../models/message.models.js";
+import { emitSocketEvent } from "../socket/index.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResPonse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+import {
+  getLocalPath,
+  getStaticFilePath,
+  removeLocalFile,
+} from "../utils/helpers.js";
+
+const chatMessageCommonAggregation = () => {
+  return [
+    {
+      $lookup: {
+        from: "users",
+        foreignField: "_id",
+        localField: "sender",
+        as: "sender",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              avatar: 1,
+              email: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        sender: { $first: "$sender" },
+      },
+    },
+  ];
+};
+
+const getAllMessages = asyncHandler(async (req, res) => {
+  const { chatId } = req.params;
+
+  const selectedChat = await Chat.findById(chatId);
+
+  if (!selectedChat) {
+    throw new ApiError(404, "Chat does not exist");
+  }
+
+  if (!selectedChat.participants?.includes(req.user?._id)) {
+    throw new ApiError(400, "User is not a part of this chat");
+  }
+
+  const message = await ChatMessage.aggregate([
+    {
+      $match: {
+        chat: new mongoose.Types.ObjectId(chatId),
+      },
+    },
+    ...chatMessageCommonAggregation(),
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(new ApiResPonse(200, message || [], "Message fetched successfully"));
+});
+
+const sendMessages = asyncHandler(async (req, res) => {
+  const { chatId } = req.params;
+  const { content } = req.body;
+
+  if (!content && !req.files?.attachments?.length) {
+    throw new ApiError(400, "Message content or attachment is required");
+  }
+
+  const selectedChat = await Chat.findById(chatId);
+
+  if (!selectedChat) {
+    throw new ApiError(404, "Chat does not exist");
+  }
+
+  const messageFiles = [];
+
+  if (req.files && req.files.attachments?.length > 0) {
+    req.files.attachments?.map((attachment) => {
+      messageFiles.push({
+        url: getStaticFilePath(req, attachment.filename),
+        localPath: getLocalPath(attachment.filename),
+      });
+    });
+  }
+
+  const message = await ChatMessage.create({
+    sender: new mongoose.Types.ObjectId(req.user._id),
+    content: content || "",
+    chat: new mongoose.Types.ObjectId(chatId),
+    attachments: messageFiles,
+  });
+
+  const chat = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $set: {
+        lastMessage: message._id,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const messages = await ChatMessage.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(message._id),
+      },
+    },
+    ...chatMessageCommonAggregation(),
+  ]);
+
+  const receivedMessage = message[0];
+
+  if (!receivedMessage) {
+    throw new ApiError(500, "Internal server error");
+  }
+
+  chat.participants.forEach((participantObjectId) => {
+    if (participantObjectId.toString() === req.user._id.toString()) return;
+
+    emitSocketEvent(
+      req,
+      participantObjectId.toString(),
+      ChatEventEnum.MESSAGE_RECEIVED_EVENT,
+      receivedMessage
+    );
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResPonse(201, receivedMessage, "Message saved successfully"));
+});
+
+const deleteMessage = asyncHandler(async (req, res) => {
+  const { chatId, messageId } = req.params;
+
+  const chat = await Chat.findOne({
+    _id: new mongoose.Types.ObjectId(chatId),
+    participants: req.user?._id,
+  });
+
+  if (!chat) {
+    throw new ApiError(404, "Chat does not exist");
+  }
+
+  const message = await ChatMessage.findOne({
+    _id: new mongoose.Types.ObjectId(messageId),
+  });
+
+  if (!message) {
+    throw new ApiError(404, "Message does not exist");
+  }
+
+  if (message.sender.toString() !== req.user._id.toString()) {
+    throw new ApiError(
+      403,
+      "You are not the authorized to delete the message, you are not the sender"
+    );
+  }
+
+  if (message.attachments.length > 0) {
+    message.attachments.map((asset) => {
+      removeLocalFile(asset.localPath);
+    });
+  }
+
+  await ChatMessage.deleteOne({
+    _id: new mongoose.Types.ObjectId(messageId),
+  });
+
+  if (chat.lastMessage.toString() === message._id.toString()) {
+    const lastMessage = await ChatMessage.findOne(
+      { chat: chatId },
+      {},
+      { sort: { createdAt: -1 } }
+    );
+
+    await Chat.findByIdAndUpdate(chatId, {
+      lastMessage: lastMessage ? lastMessage?._id : null,
+    });
+  }
+
+  chat.participants.forEach((participantObjectId) => {
+    if (participantObjectId.toString() === req.user._id.toString()) return;
+
+    emitSocketEvent(
+      req,
+      participantObjectId.toString(),
+      ChatEventEnum.MESSAGE_DELETE_EVENT
+    );
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResPonse(200, message, "Message deleted successfully"));
+});
+
+export { getAllMessages, sendMessages, deleteMessage };
