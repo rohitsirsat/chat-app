e36ae29fc0d969e6ad53157d3d1b6487d18cf00d
rohit <rohitsirsat855@gmail.com>
2024-07-08 11:05:18 +0530

    add constants

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -15,27 +15,39 @@ export const UserLoginType = {
 
 export const AvailableSocialLogins = Object.values(UserLoginType);
 
+// set of events that we are using in chat app.
+
 export const ChatEventEnum = Object.freeze({
   // ? once user is ready to go
   CONNECTED_EVENT: "connected",
+
   // ? when user gets disconnected
   DISCONNECT_EVENT: "disconnect",
+
   // ? when user joins a socket room
   JOIN_CHAT_EVENT: "joinChat",
+
   // ? when participant gets removed from group, chat gets deleted or leaves a group
   LEAVE_CHAT_EVENT: "leaveChat",
+
   // ? when admin updates a group name
   UPDATE_GROUP_NAME_EVENT: "updateGroupName",
+
   // ? when new message is received
   MESSAGE_RECEIVED_EVENT: "messageReceived",
+
   // ? when there is new one on one chat, new group chat or user gets added in the group
   NEW_CHAT_EVENT: "newChat",
+
   // ? when there is an error in socket
   SOCKET_ERROR_EVENT: "socketError",
+
   // ? when participant stops typing
   STOP_TYPING_EVENT: "stopTyping",
+
   // ? when participant starts typing
   TYPING_EVENT: "typing",
+
   // ? when message is deleted
   MESSAGE_DELETE_EVENT: "messageDeleted",
 });
